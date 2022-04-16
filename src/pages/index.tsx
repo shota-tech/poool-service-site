03ac7fe75ebcaaf93e5bfd03b3b2e00add677301
shota-tech <iwmtsht@gmail.com>
2022-04-16 19:24:50 +0900

    initial settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Box, Text } from '@chakra-ui/react'
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+      <Text>Hello World!</Text>
+    </Box>
+  )
+}
+
+export default Home
